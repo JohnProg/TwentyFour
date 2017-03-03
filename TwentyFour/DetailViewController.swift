873552f7a7fe7a17fc946399d5c0a4a72c4d5330
Andrea Miotto <andreamiotto@macbook-pro-andrea-miotto.local>
2017-03-03 18:25:15 +0100

    designed interface for details

--- TwentyFour/DetailViewController.swift
+++ TwentyFour/DetailViewController.swift
@@ -2,44 +2,42 @@
 //  DetailViewController.swift
 //  TwentyFour
 //
-//  Created by Andrea Miotto on 24/02/17.
+//  Created by Andrea Miotto on 03/03/17.
 //  Copyright © 2017 Andrea Miotto. All rights reserved.
 //
 
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.dateCreation!.description
-            }
-        }
-    }
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var moodIconView: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var locationStackView: UIStackView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var updateStackView: UIStackView!
+    @IBOutlet weak var updateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.configureView()
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-    var detailItem: JournalEntry? {
-        didSet {
-            // Update the view.
-            self.configureView()
-        }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-
+    */
 
 }
-
